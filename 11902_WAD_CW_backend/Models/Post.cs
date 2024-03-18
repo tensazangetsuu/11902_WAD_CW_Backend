2a94e1834eaddfd9aa01e533a1f1d0a90e404a31
Shakhzod Bobokulov <00011902@wiut.uz>
2024-03-18 18:31:43 +0500

    Added base classes

---
+++ 11902_WAD_CW_backend/Models/Post.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace _11902_WAD_CW_backend.Models
+{
+
+    public class Post
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Content is required")]
+        public string? Content { get; set; }
+
+        // Nullable foreign key
+        public int? TagID { get; set; }
+
+        // Navigation property
+        [ForeignKey("TagID")]
+        public Tag? Tag { get; set; }
+    }
+}
---
+++ 11902_WAD_CW_backend/Models/Tag.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _11902_WAD_CW_backend.Models
+{
+    public class Tag
+    {
+        public int ID { get; set; }
+        private string _title;
+        [Required(ErrorMessage = "Name is required")]
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Tag title cannot be null or empty.");
+                }
+
+                _title = value;
+            }
+        }
+    }
+}
